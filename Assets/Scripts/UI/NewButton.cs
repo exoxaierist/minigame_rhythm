337f1f7fc8598ba4b193d1081caf1868ce3b9b31
exoxaierist <exoxaierist@naver.com>
2023-05-04 02:26:27 +0900

    버튼, 소리대충

---
+++ Assets/Scripts/UI/NewButton.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(AudioSource))]
+public class NewButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    private Image image;
+    private AudioSource audioSrc;
+    public Sprite defaultSprite;
+    public Sprite hoverSprite;
+    public Sprite pressedSprite;
+
+    public AudioClip hoverClip;
+    public AudioClip pressClip;
+
+    private float pressedDuration = 0.1f;
+    private bool hover = false;
+
+    public UnityEvent buttonEvent;
+    private Coroutine pressedCoroutine;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        audioSrc = GetComponent<AudioSource>();
+        audioSrc.playOnAwake = false;
+        audioSrc.bypassListenerEffects = true;
+        audioSrc.bypassReverbZones = true;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (pressedCoroutine != null) return;
+        buttonEvent.Invoke();
+        pressedCoroutine = StartCoroutine(SpritePressed());
+        audioSrc.clip = pressClip;
+        audioSrc.Play();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hover = true;
+        if (pressedCoroutine != null) return;
+        image.sprite = hoverSprite;
+        audioSrc.clip = hoverClip;
+        audioSrc.Play();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hover = false;
+        if (pressedCoroutine != null) return;
+        image.sprite = defaultSprite;
+    }
+
+    private IEnumerator SpritePressed()
+    {
+        image.sprite = pressedSprite;
+        yield return new WaitForSecondsRealtime(pressedDuration);
+        if (hover) image.sprite = hoverSprite;
+        else image.sprite = defaultSprite;
+        pressedCoroutine = null;
+    }
+}
