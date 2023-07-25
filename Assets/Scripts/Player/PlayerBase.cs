78049fbe6903535c32e9bd0ce814820212b42ebd
jsewo9119 <70896242+jsewo9119@users.noreply.github.com>
2023-07-25 12:27:54 +0900

    문 튕겨져 나오기

--- Assets/Scripts/Player/PlayerBase.cs
+++ Assets/Scripts/Player/PlayerBase.cs
@@ -3,16 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 유닛 개발 기반
+// ???? ???? ????
 public class PlayerBase : ControlledObject
 {
     [HideInInspector] public Hp hp;
 
     public bool fainted = false;
 
+    List<string> movemem = new List<string>();
+
     protected override void Awake()
     {
         base.Awake();
+        movemem.Add("start");
         CheckForHP();
         if (player == Player.Player1) collisionLayer = Global.p1MoveColMask;
         else collisionLayer = Global.p2MoveColMask;
@@ -26,21 +29,29 @@ public class PlayerBase : ControlledObject
     protected override void MoveUp()
     {
         Move(new(0, 1));
+        movemem.RemoveAt(0);
+        movemem.Add("MoveUp");
     }
 
     protected override void MoveDown()
     {
         Move(new(0, -1));
+        movemem.RemoveAt(0);
+        movemem.Add("MoveDown");
     }
 
     protected override void MoveRight()
     {
         Move(new(1, 0));
+        movemem.RemoveAt(0);
+        movemem.Add("MoveRight");
     }
 
     protected override void MoveLeft()
     {
         Move(new(-1,0));
+        movemem.RemoveAt(0);
+        movemem.Add("MoveLeft");
     }
 
     private void Move(Vector2 target)
@@ -56,7 +67,7 @@ public class PlayerBase : ControlledObject
         }
     }
 
-    // 오브젝트에 같이 딸려있는 HP 컴포넌트 찾아서 이벤트 등록
+    // ?????????? ???? ???????? HP ???????? ?????? ?????? ????
     protected void CheckForHP()
     {
         if (TryGetComponent(out hp))
@@ -64,6 +75,27 @@ public class PlayerBase : ControlledObject
             hp.OnHeal += OnHeal;
             hp.OnDamage += OnDamage;
             hp.OnDeath += OnDeath;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            switch (movemem[0])
+            {
+                case "MoveUp":
+                    MoveDown();
+                    break;
+                case "MoveDown":
+                    MoveUp();
+                    break;
+                case "MoveRight":
+                    MoveLeft();
+                    break;
+                case "MoveLeft":
+                    MoveRight();
+                    break;
+            }
         }
     }
 }
